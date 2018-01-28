bc70d77de9671cdeee8ad0cc48b3f1774b72f2cf
Aluisio Martins Junior <junior@mjpsolucoes.com.br>
2018-01-28 10:03:57 -0200

    Atualização do arquivo gulp, passa a utiliza pump() no lugar de pipe()

--- gulpfile.js
+++ gulpfile.js
@@ -3,71 +3,86 @@ var jshint      = require('gulp-jshint');
 var clean       = require('gulp-clean');
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
-var es          = require('event-stream');
 var htmlmin     = require('gulp-htmlmin');
 var sass        = require('gulp-sass');
 var cleanCSS    = require('gulp-clean-css');
 var livereload  = require('gulp-livereload');
 var runSequence = require('run-sequence');
-var rename      = require('gulp-rename');
+var pump        = require('pump');
 
-gulp.task('clean', function ()
-{
+/* Utils */
+//var es     = require('event-stream');
+//var rename = require('gulp-rename');
+
+gulp.task('clean', function () {
     return gulp.src('public/').pipe(clean());
 });
 
-gulp.task('jshint', function ()
-{
-    return gulp.src('assets/js/**/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+gulp.task('lint', function (cb) {
+    pump([
+            gulp.src('assets/js/**/*.js'),
+            jshint(),
+            jshint.reporter('default')
+        ],
+        cb
+    );
 });
 
-gulp.task('uglify', function ()
-{
-    return es.merge([
-        gulp.src(['assets/js/**/*.js']).pipe(concat('scripts.js')).pipe(uglify())
-    ])
-        .pipe(concat('all.min.js'))
-        .pipe(gulp.dest('public/js'))
-        .pipe(livereload());
+gulp.task('compress', function (cb) {
+    pump([
+            gulp.src('assets/js/**/*.js'),
+            concat('scripts.js'),
+            uglify(),
+            concat('all.min.js'),
+            gulp.dest('public/js'),
+            livereload()
+        ],
+        cb
+    );
 });
 
-gulp.task('htmlmin', function ()
-{
-    return gulp.src('assets/views/**/*.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('public/views'))
-        .pipe(livereload());
+gulp.task('htmlmin', function (cb) {
+    pump([
+            gulp.src('assets/views/**/*.html'),
+            htmlmin({collapseWhitespace: true}),
+            gulp.dest('public/views'),
+            livereload()
+        ],
+        cb
+    );
 });
 
-gulp.task('sass', function ()
-{
-    return gulp.src('assets/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('assets/css'));
+gulp.task('sass', function (cb) {
+    pump([
+            gulp.src('assets/sass/**/*.scss'),
+            sass().on('error', sass.logError),
+            gulp.dest('assets/css')
+        ],
+        cb
+    );
 });
 
-gulp.task('cssmin', function ()
-{
-    return gulp.src(['assets/css/**/*.css'])
-        .pipe(cleanCSS())
-        .pipe(concat('styles.css'))
-        .pipe(gulp.dest('public/css'))
-        .pipe(livereload());
+gulp.task('cssmin', function (cb) {
+    pump([
+            gulp.src(['assets/css/**/*.css']),
+            cleanCSS(),
+            concat('styles.css'),
+            gulp.dest('public/css'),
+            livereload()
+        ],
+        cb
+    );
 });
 
-gulp.task('watch', function ()
-{
+gulp.task('watch', function () {
     livereload.listen();
     gulp.watch('assets/views/*.html', ['htmlmin']);
     gulp.watch('assets/views/**/*.html', ['htmlmin']);
-    gulp.watch('assets/js/**/*.js', ['uglify']);
+    gulp.watch('assets/js/**/*.js', ['compress']);
     gulp.watch('assets/sass/**/*.scss', ['sass']);
     gulp.watch('assets/css/**/*.css', ['cssmin']);
 });
 
-gulp.task('default', function (cb)
-{
-    return runSequence('clean', ['jshint', 'uglify', 'htmlmin', 'sass', 'cssmin', 'watch'], cb)
+gulp.task('default', function (cb) {
+    return runSequence('clean', ['lint', 'compress', 'htmlmin', 'sass', 'cssmin', 'watch'], cb)
 });
