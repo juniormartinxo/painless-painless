e4f1be69aa6c8473824064cbc5d819336bc48562
Junior Martins <amjr.box@gmail.com>
2020-09-04 21:05:59 -0300

    Criação do config da autenticação

---
+++ config/authenticate.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Martins Junior
+ * Date: 2020/09/04
+ * Time: 08:24
+ */
+
+return [
+    'CONFIG_AUTHENTICATE' => $_ENV['CONFIG_AUTHENTICATE'] ?? true
+];
